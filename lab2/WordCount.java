bea490766c2ca8a199d112ff4d00ba63ca6d6d9f
lab3 <lab3@localhost.localdomain>
2017-12-18 17:48:24 +0530

    Added lab2 word count program

---
+++ lab2/WordCount.java
@@ -0,0 +1,56 @@
+package com.wordCount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount{
+	public static class Map extends Mapper<Object, Text, Text, IntWritable>{
+		private final static IntWritable One = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+			StringTokenizer st = new StringTokenizer(value.toString());
+			while(st.hasMoreTokens()){
+				word.set(st.nextToken());
+				context.write(word,one);
+			}
+
+		}
+
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>{
+		public void reduce(Text key, Iterable<IntWritable> values, Context contest) throws IOException,InterruptedException{
+			int sum = 0;
+			for(IntWritable val: values){
+				sum += val.get();
+			}
+			conetxt.write(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		Configuration conf = new Configuration();
+		Job job = new Job(conf,"wordcount");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(Map.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setReducerClass(Reduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0));
+		FileInputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+	}
+}
